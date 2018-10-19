141d8b1ae7507464fce7fdc2622cd38e70a601a0
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-10-19 02:06:34 +0200

    Die Übersichtsseite entsteht

--- src/song-overview/song-overview.js
+++ src/song-overview/song-overview.js
@@ -46,6 +46,10 @@ class SongOverview {
      */
     constructor(app) {
         this._app = app;
+
+        // Suche und Sortierung
+        this._order = "title";
+        this._query = "";
     }
 
     /**
@@ -59,6 +63,12 @@ class SongOverview {
      */
     async onShow() {
         let section = document.querySelector("#song-overview").cloneNode(true);
+
+        let order = "title";
+        let query = "";
+
+        let songs = await this._searchSongs(query, order);
+        this._renderList(songs, order, section.querySelector("main > ul"));
 
         return {
             className: "song-overview",
@@ -85,6 +95,159 @@ class SongOverview {
      */
     get title() {
         return "Übersicht";
+    }
+
+    /**
+     * Diese Methode greift auf die Datenbank zu und sucht die gespeicherten
+     * Songs. Optional kann ein Suchbegriff mitgegeben werden, der innerhalb
+     * des Songtitels oder des Künstlernamens vorkommen muss. Ebenso kann
+     * einer von folgenden Werten mitgegeben werden, um die Ergebnisliste zu
+     * sortieren:
+     *
+     *   * "title": Sortierung nach Songtitel und Künstler
+     *   * "artist": Sortierung nach Künstler und Songtitel
+     *
+     * Die Ergebnisliste hat folgendes Format:
+     *
+     * [
+     *      {
+     *          id: "Datenbank-ID",                 // Schlüsselwert der Datenbank
+     *          artist: "Name des Künstlers",       // Name des Künstlers
+     *          title: "Name des Songs",            // Name des Songs
+     *          format: "html",                     // Bisher immer "html"
+     *          data: "HTML-String",                // HTML-String mit Songinhalt
+     *      }, {
+     *        …
+     *      }
+     * ]
+     *
+     * @param  {String} query Suchbegriff (optional)
+     * @param  {String} sort Sortierung (optional)
+     * @return {Array} Liste der gefundenen Songs
+     */
+    async _searchSongs(query, sort) {
+        // Songs suchen
+        let songtexts = new Database.Songtexts();
+        let songs = await songtexts.search(query);
+
+        // Ergebnis sortieren
+        songs.sort((lhs, rhs) => {
+            let resultArtist = lhs.artist.localeCompare(rhs.artist);
+            let resultTitle = lhs.title.localeCompare(rhs.title);
+
+            if (sort === "artist") {
+                // Sortierung nach Künstler und Songtitel
+                if (resultArtist != 0) {
+                    return resultArtist;
+                } else {
+                    return resultTitle;
+                }
+            } else {
+                // Sortierung nach Songtitel und Künstler
+                if (resultTitle != 0) {
+                    return resultTitle;
+                } else {
+                    return resultArtist;
+                }
+            }
+        });
+
+        return songs;
+    }
+
+    /**
+     * Diese Methode nimmt die von searchSongs() generierte Liste mit Songtexten
+     * entgegen und zeigt sie auf der Seite an. Hierfür muss der Methode über
+     * den Parameter groupBy einer von folgenden beiden Strings mitgegeben
+     * werden:
+     *
+     *   * "title": Gruppierung anhand erstem Buchstaben des Titels
+     *   * "artist": Gruppierung anhand des Künstlers
+     *
+     * Im Parameter parentNode muss das <ul>-Element übergeben werden, in
+     * welches die Listeneinträge eingefügt werden sollen.
+     *
+     * @param {Array} songs Liste der darzustellenden Songs
+     * @param {String} groupBy Kriterium für die Zwischenüberschriften
+     * @param {HTMLNode} parentNode <ul>-Element der Liste
+     */
+    _renderList(songs, groupBy, parentNode) {
+        parentNode.innerHTML = "";
+
+        if (songs.length < 1) {
+            // Hinweistext, wenn noch keine Songs vorhanden sind
+            parentNode.innerHTML += `
+                <li>
+                    <div class="padding no-data">
+                        Noch keine Texte vorhanden
+                    </div>
+                </li>
+            `;
+        } else {
+            // Zwischenüberschriften und Songtexte
+            let currentGroup = "";
+
+            songs.forEach(song => {
+                // Zwischenüberschrift zur Gruppierung der Songs
+                //
+                // <li data-section-title="A">
+                //     <div class="section-title">
+                //         A
+                //     </div>
+                // </li>
+                let songGroup1 = "";
+                let songGroup2 = "";
+
+                if (groupBy === "title") {
+                    songGroup1 = song.title.trim()[0].toUpperCase();
+                    songGroup2 = songGroup1;
+                } else {
+                    songGroup1 = song.artist.trim();
+                    songGroup2 = songGroup1.toUpperCase();
+                }
+
+                if (currentGroup != songGroup2) {
+                    currentGroup = songGroup2;
+
+                    let liGroup = document.createElement("li");
+                    liGroup.dataset.sectionTitle = songGroup1;
+
+                    let divGroup = document.createElement("div");
+                    divGroup.classList.add("section-title");
+                    divGroup.textContent = songGroup1;
+
+                    parentNode.appendChild(liGroup);
+                    liGroup.appendChild(divGroup);
+                }
+
+                // Der eigentliche Song
+                //
+                // <li class="entry" data-song-title="Another Day In Paradise" data-song-artist="Phil Collins">
+                //     <div class="song-title">
+                //         Another Day In Paradise
+                //     </div>
+                //     <div class="song-artist">
+                //         Phil Collins
+                //     </div>
+                // </li>
+                let liSong = document.createElement("li");
+                liSong.classList.add("entry");
+                liSong.dataset.songTitle = song.title.trim();
+                liSong.dataset.songArtist = song.artist.trim();
+
+                let divTitle = document.createElement("div");
+                divTitle.classList.add("song-title");
+                divTitle.textContent = song.title.trim();
+
+                let divArtist = document.createElement("div");
+                divArtist.classList.add("song-artist");
+                divArtist.textContent = song.artist.trim();
+
+                parentNode.appendChild(liSong);
+                liSong.appendChild(divTitle);
+                liSong.appendChild(divArtist);
+            });
+        }
     }
 }
 
